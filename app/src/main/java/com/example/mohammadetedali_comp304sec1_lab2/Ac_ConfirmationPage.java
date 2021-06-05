621fed4c56d5724bc77d754d272a38c07b78b4f5
etedali <etedali@gmail.com>
2021-06-05 00:19:55 -0400

    add confirm page

---
+++ app/src/main/java/com/example/mohammadetedali_comp304sec1_lab2/Ac_ConfirmationPage.java
@@ -0,0 +1,14 @@
+package com.example.mohammadetedali_comp304sec1_lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ac_ConfirmationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac_confirmation_page);
+    }
+}
---
+++ app/src/main/java/com/example/mohammadetedali_comp304sec1_lab2/Ac_CustomerInformation.java
@@ -0,0 +1,18 @@
+package com.example.mohammadetedali_comp304sec1_lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Ac_CustomerInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac_customer_information);
+    }
+
+    public void onConfirmClick(View view) {
+    }
+}
